92ce7f446e0c6033722ae0def8baaed1a5f50bb6
Alex <alexanderE1992@hotmail.com>
2024-01-03 15:52:42 +0100

    Nu fungerar det att starta applikationen och man ser en grid

--- SnakeProjekt/Images.cs
+++ SnakeProjekt/Images.cs
@@ -6,21 +6,16 @@ namespace SnakeProjekt
 {
 	public static class Images
 	{
-	 public static readonly string SnakeHead = "SnakeHead.png";
-		public static readonly string SnakeBody = "SnakeBody.png";
-			
-		public static readonly string SnakeDeadHead = "SnakeDeadHead.png";
-		public static readonly string SnakeDeadBody = "SnakeDeadBody.png";
-
-		public static readonly string Food = "Food.png";
-		public static readonly string Empty = "EmptySpace.png";
-		
-
+		public static readonly ImageSource SnakeHead = LoadImage("SnakeHead.png");
+		public static readonly ImageSource SnakeBody = LoadImage("SnakeBody.png");
+		public static readonly ImageSource SnakeDeadHead = LoadImage("SnakeHeadDead.png");
+		public static readonly ImageSource SnakeDeadBody = LoadImage("SnakeBodyDead.png");
+		public static readonly ImageSource Food = LoadImage("Food.png");
+		public static readonly ImageSource Empty = LoadImage("EmptySpace.png");
 
 		private static ImageSource LoadImage(string fileName)
 		{
-			return new BitmapImage(new Uri($"Assets/{fileName}", UriKind.Relative));
+			return new BitmapImage(new Uri($"pack://application:,,,/Assets/{fileName}", UriKind.Absolute));
 		}
-
 	}
 }
--- SnakeProjekt/MainWindow.xaml.cs
+++ SnakeProjekt/MainWindow.xaml.cs
@@ -11,14 +11,41 @@ using System.Windows.Shapes;
 
 namespace SnakeProjekt
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
+
+		private readonly int rows = 20, cols = 20;
+		private readonly Image[,] GridImages;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			GridImages = SetupGrid();
+		}
+
+		private Image[,] SetupGrid()
+		{
+			Image[,] images = new Image[rows, cols];
+			GameGrid.Rows = rows;
+			GameGrid.Columns = cols;
+
+			for (int r = 0; r < rows; r++)
+			{
+				 for (int c = 0; c < cols; c++)
+				{
+					
+						Image image = new Image
+						{
+							Source = Images.Empty
+						};
+
+					
+
+					images[r, c] = image;
+					GameGrid.Children.Add(image);
+				 }
+			}
+			return images;
 		}
 	}
 }
